0eed715bab400d91968f33b2f5fc3d10e9cd0a74
Johanx22x <johanrodsa2210@gmail.com>
2026-08-01 13:44:38 -0600

    Fix markdown-preview build command
    mkdp#util#install() downloads a prebuilt binary that does not always
    resolve. Building from app/install.sh is the documented fallback.

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,9 +2,7 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = "markdown",
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
+  build = "cd app && ./install.sh",
   init = function()
     vim.g.mkdp_browser = "firefox"
     vim.g.mkdp_theme = "dark"
